0b260d88c20f7137dd8ed47c98b9b21de8a7f988
df <wudongfei@gmail.com>
2016-12-21 11:19:23 +0800

    init

--- setup-sample.sh
+++ setup-sample.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 bosh -e e upload-release ../utils/on-demand-service-broker-0.12.4.tgz
-#bosh -e e upload-release dev_releases/redis-service-adapter/`ls -1  dev_releases/redis-service-adapter/|sort -t. -k2,2 -nr|head -1` 
+bosh -e e upload-release dev_releases/redis-service-adapter/`ls -1  dev_releases/redis-service-adapter/|sort -t. -k2,2 -nr|head -1` 
+
+bosh -e e -d redis-broker deploy man
